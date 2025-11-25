d8c867775044bec9f823fcec992a7e8956cb8e57
LeeMinJae <lmj201212@gmail.com>
2025-11-25 22:19:15 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -5,24 +5,6 @@ using namespace std;
 
 string solution(string my_string, string overwrite_string, int s) {
     string answer = "";
-    for(int i =0;i<1000;i++)
-    {
-        if(my_string[i]==0)
-            break;
-        answer.push_back(my_string[i]);
-        if(i==s)
-        {
-            answer.pop_back();
-            for(int j=0;j<1000;j++)
-            {
-                if(overwrite_string[j]==0)
-                    break; 
-                answer.push_back(overwrite_string[j]);
-                i++;
-            }
-            i--;
-        }
-        
-    }
+    answer=my_string.replace(s,overwrite_string.size(),overwrite_string);
     return answer;
 }
