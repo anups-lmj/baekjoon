1b1e70f2073c97f20ac09a7d83e23fba5c73f36a
LeeMinJae <lmj201212@gmail.com>
2025-12-10 22:56:47 +0900

    [level 0] Title: 수 조작하기 2, Time: 1.65 ms, Memory: 6.86 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    for(int i=0; i<numLog.size()-1;i++)
+    {
+        int num = numLog[i]-numLog[i+1];
+        switch(num)
+        {
+            case -1:
+                answer += 'w';
+                break;
+            case -10:
+                answer += 'd';
+                break;
+            case 1:
+                answer += 's';
+                break;
+            case 10:
+                answer += 'a';
+                break;
+        }
+        
+    }
+    return answer;
+}
