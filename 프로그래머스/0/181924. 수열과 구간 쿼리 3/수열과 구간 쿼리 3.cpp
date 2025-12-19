ce4de5c8dbec88514c304ab4a422d36f71d3b343
LeeMinJae <lmj201212@gmail.com>
2025-12-19 17:01:08 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.32 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer(arr.size());
+    int temp;
+    for(int i=0;i<queries.size();i++)
+    {
+        temp = arr[queries[i][0]];
+        arr[queries[i][0]] = arr[queries[i][1]];
+        arr[queries[i][1]] = temp;
+        
+    }
+    return arr;
+}
